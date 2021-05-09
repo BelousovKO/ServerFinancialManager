c976ee91f9e5eb005e21e562a02e9761fa027960
belousovko <working.belousovko@gmail.com>
2021-05-09 20:12:38 +0700

    реализовал добавление расходов

--- routes/costs.js
+++ routes/costs.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const costsController = require('../controllers/costs');
 
-// POST /interface/new
+// POST /costs/new
 router.post('/new', costsController.create);
 
 module.exports = router;
